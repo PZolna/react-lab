b968901696385fcbe94d7148911ceb23ef560964
pzolna <patrycja.zln@gmail.com>
2025-12-14 11:49:17 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let title = 'Wall-E';
+      const [title, setTitle] = useState('Wall-E');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setTitle(event.target.value);
     }
+
+let message ='';
+if (title.length < 5) {
+    message = "Title is toos short!";
+} else if (title.length < 15) {
+    message = "Title is OK.";
+} else {
+    message = "Title is too long!";
+}
+// ...
 
     return (
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
+           {
+               title.length > 0 && <div>{message}</div>
+           g}
             <input type="text" onChange={handleChange}/>
         </div>
     );
